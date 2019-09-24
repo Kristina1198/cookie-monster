aa8c7e96edf94cd93ad412ebfd65811a0dd0e595
Kristina1198 <Kristina1198@yahoo.com>
2019-09-24 00:40:29 +0000

    validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,15 @@
 class Place < ApplicationRecord
     belongs_to :user
     validates :name, presence: true
+    validates :name, length: { minimum: 3 }
+end
+
+class Place < ApplicationRecord
+    belongs_to :user
+    validates :address, presence: true
+end
+
+class Place < ApplicationRecord
+    belongs_to :user
+    validates :description, presence: true
 end
